0f918cd3969251296ea21fc7cc54f7d7e02ff536
rashidsarkar <rashidrock558@gmail.com>
2023-11-06 21:31:08 +0600

    Updated the Axios base URL to https://educoda-server.vercel.app for making API requests to the new server.

--- src/AxiosAPI/axiosInstance.js
+++ src/AxiosAPI/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000", // Your API base URL
+  baseURL: "https://educoda-server.vercel.app", // Your API base URL
   withCredentials: true,
 });
 export default axiosInstance;
